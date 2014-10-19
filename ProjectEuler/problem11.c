960b77a095878b00bb19c169d5e3030933f5cc11
frankleveque <leveque.frank@gmail.com>
2014-10-18 22:32:53 -0700

    Add 11th Project Euler Problem

--- ProjectEuler/problem11.c
+++ ProjectEuler/problem11.c
@@ -36,9 +36,7 @@ What is the greatest product of four adjacent numbers in the same direction
 (up, down, left, right, or diagonally) in the 20×20 grid?
 \endcode
  *
- *
  */
-
 
 #include <stdio.h>
 
